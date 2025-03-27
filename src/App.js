3afed5854455407b79038bdfdf3f8d9258d1d6c2
Mark Polichette <Mark@polichette.com>
2025-03-27 14:15:09 -0600

    another publishing troubleshoot

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Automapper from "./pages/Automapper.js";
 import Home from "./pages/home.js";
